197f4681e5ec43bb044a4eaad734bdc5cd5abab4
jacobliberman <jacobliberman@gmail.com>
2015-10-04 16:13:17 -0500

    scrub london scores

--- func/poss_func.R
+++ func/poss_func.R
@@ -47,14 +47,14 @@ match_score <- function(poss) {
 }
 
 # returns a data frame of possession data
-poss_filter <- function(matches=1:315, team="all", event="all", round="all") { 
+poss_filter <- function(matches=1:405, team="all", event="all", round="all") { 
   
   # download the data file and save it to an object
   if (!file.exists("ws72014_poss_8.csv")) {
     download.file("https://raw.githubusercontent.com/jliberma/rugby7s/master/data/ws72014_possession_stats.csv", 
-                  destfile="ws72014_poss_8.csv", method="curl")
+                  destfile="ws72014_poss.csv", method="curl")
   }
-  ws7s_poss <- read.csv("ws72014_poss_8.csv", header=TRUE, stringsAsFactors=FALSE)
+  ws7s_poss <- read.csv("ws72014_poss.csv", header=TRUE, stringsAsFactors=FALSE)
   
   # validate and subset by event
   if (event != "all") { 
