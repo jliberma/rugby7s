f68d010a29e6abce9809d619e86c251d06fd6ee9
Jacob Liberman <jliberma@redhat.com>
2017-12-29 00:26:28 -0600

    update possession graphs

--- 08_poss/poss_full.R
+++ 08_poss/poss_full.R
@@ -19,16 +19,24 @@ ws7s_poss <- read.csv("ws72014_poss.csv", header=TRUE, stringsAsFactors=FALSE)
 poss.per.match <- sapply(split(ws7s_poss,ws7s_poss$SERIES.MATCH), 
                          function(x) nrow(x))
 summary(poss.per.match)
+boxplot(poss.per.match)
 
+# possessions per match by team
+# possessions per match by round
+# boxplot ppm by team/round
+
+# create a histogram of possessions per match
 ppm <- qplot(poss.per.match, binwidth=1) + 
   geom_vline(x=median(poss.per.match)+.5, colour="red") + 
   xlab("") + ylab("")
 
+# create a density function of possessions per match
 cdf <- ggplot(as.data.frame(poss.per.match), 
               aes(x=poss.per.match)) +
   stat_ecdf(geom="smooth") + 
   xlab("") + ylab("")
 
+# plot them side by side
 grid.arrange(ppm, cdf, ncol=2)
 
 # possessions per team per match
@@ -39,16 +47,22 @@ all_scores <- match_score_poss(ws7s_poss)
 # find winning possession margin
 wpm <- all_scores %>% 
   filter(! points.1 == points.2) %>%
+  #filter(points.1 == points.2) %>%
   mutate(wpm = ifelse(points.1 > points.2, 
                       count.1-count.2, 
                       count.2-count.1), 
-         pos = wpm >=0) %>%
+         pos = wpm >= 0) %>%
   select(match, wpm, pos) %>%
   arrange(wpm)
+# QUESTION: add ties to the analysis with pos false but abs(wpm)
 
 summary(wpm[,2:3])
 # on average, the winning team has 9 possessions
 # versus 8 for the losing team
+# QUESTION: what is this percentage?
+
+# ties
+# 9 ties: 2 1 1 1 1 1 0 0
 
 # graph unsorted wpms by match
 wpm1 <- ggplot(wpm, aes(x=match, y=wpm, fill=pos)) +
@@ -74,14 +88,22 @@ for (i in 1:3)
   print(paste("wpm >=",i,":",j))
 }
 
-```{r, echo=FALSE}
-tie_scores <- all_scores %>% 
-  filter(points.1==points.2) %>%
-  transmute(abs(count.1-count.2))
-```
+# Match count at each wpm
+l_total = data.frame()
+for (i in sort(unique(abs(wpm$wpm))))
+{
+  # how to save this to a vector to be displayed later?
+  j <- sum(abs(wpm$wpm)==i) + length(tie_scores[tie_scores==i])
+  k <- round(sum(wpm$wpm==i)/(sum(abs(wpm$wpm)==i) +
+                                + length(tie_scores[tie_scores==i])),2)
+  print(paste(i,j,k))
+  l <- data.frame(i,j,k)
+  l_total <- rbind(l,l_total)
+}
 
 # create data frame of team possessions, opponent possessions, and 
 # number of matches
+# replace all of this crap with dataframe operations
 poss.per.team <- sapply(split(ws7s_poss,ws7s_poss$TEAM), 
                         function(x) nrow(x))
 poss.per.opp <- sapply(split(ws7s_poss,ws7s_poss$OPP), 
@@ -93,15 +115,16 @@ team.poss <- as.data.frame(cbind(poss.per.team,
 team.poss <- cbind(team = factor(names(poss.per.team)), 
                    team.poss)
 
-# calculate possession differential by team
+
+# calculate total possession differential by team
 team.poss.diff <- team.poss %>% 
-  mutate(diff = round((poss.per.team-poss.per.opp)/match.per.team,2)) %>%
+  mutate(diff = round(poss.per.team-poss.per.opp,1)) %>%
   select(team, diff) %>%
   arrange(-diff)
 
 # order factor levels for plotting
 team.poss.diff$team <- factor(team.poss.diff$team, 
-                              levels=team.poss.diff$team[order(team.poss.diff$diff)])
+                                  levels=team.poss.diff$team[order(team.poss.diff$diff)])
 
 tpd <- ggplot(team.poss.diff) + 
   geom_point(aes(x=team, y=diff), fill="gray", stat="identity") + 
@@ -112,14 +135,62 @@ tpd <- ggplot(team.poss.diff) +
   ylab("") + xlab("")
 tpd
 
-wpm <- all_scores %>% 
-  filter(! points.1 == points.2) %>%
-  mutate(wpm = ifelse(points.1 > points.2, 
-                      count.1-count.2, 
-                      count.2-count.1), 
-         pos = wpm >=0) %>%
-  select(match, wpm, pos) %>%
-  arrange(wpm)
+# calculate average possession differential by team
+avg.team.poss.diff <- team.poss %>% 
+  mutate(diff = round((poss.per.team-poss.per.opp)/match.per.team,1)) %>%
+  select(team, diff) %>%
+  arrange(-diff)
+
+# order factor levels for plotting
+avg.team.poss.diff$team <- factor(avg.team.poss.diff$team, 
+                              levels=avg.team.poss.diff$team[order(avg.team.poss.diff$diff)])
+
+# this is the meaningful stat
+# get average posession diff in cup round only
+atpd <- ggplot(avg.team.poss.diff) + 
+  geom_point(aes(x=team, y=diff), fill="gray", stat="identity") + 
+  coord_flip() +
+  theme(panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(linetype=3, color="darkgray"),
+        axis.text.y=element_text(size=rel(0.8))) +
+  ylab("") + xlab("")
+atpd
+
+# LOOK AT ATPD IN CUP ROUNDS ONLY, LABEL ON SAME GRAPH
+ws7s_poss_cup <- ws7s_poss %>%
+  filter(ROUND=="Pool" | ROUND == "Cup")
+
+# create data frame of team possessions, opponent possessions, and 
+# number of matches
+poss.per.team.cup <- sapply(split(ws7s_poss_cup,ws7s_poss_cup$TEAM), 
+                        function(x) nrow(x))
+poss.per.opp.cup <- sapply(split(ws7s_poss_cup,ws7s_poss_cup$OPP), 
+                       function(x) nrow(x))
+match.per.team.cup <- sapply(split(ws7s_poss_cup,ws7s_poss_cup$TEAM), 
+                         function(x) length(unique(x$SERIES.MATCH)))
+team.poss.cup <- as.data.frame(cbind(poss.per.team.cup, 
+                                 poss.per.opp.cup, match.per.team.cup))
+team.poss.cup <- cbind(team = factor(names(poss.per.team.cup)), 
+                   team.poss.cup)
+# calculate average possession differential by team
+avg.team.poss.diff.cup <- team.poss.cup %>% 
+  mutate(diff = round((poss.per.team.cup-poss.per.opp.cup)/match.per.team.cup,1)) %>%
+  select(team, diff) %>%
+  arrange(-diff)
+
+# order factor levels for plotting
+avg.team.poss.diff.cup$team <- factor(avg.team.poss.diff.cup$team, 
+                                  levels=avg.team.poss.diff.cup$team[order(avg.team.poss.diff.cup$diff)])
+
+# average posession diff in cup round only
+atpd_cup <- ggplot(avg.team.poss.diff.cup) + 
+  geom_point(aes(x=team, y=diff), fill="gray", stat="identity") + 
+  coord_flip() +
+  theme(panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(linetype=3, color="darkgray"),
+        axis.text.y=element_text(size=rel(0.8))) +
+  ylab("") + xlab("")
+atpd_cup
 
 # sources of possession advantage
 # reclaims, restart penalties, restart infractions
@@ -145,12 +216,14 @@ wpm <- all_scores %>%
 # also look at possession efficiency, which is the number of 
 # possessions that result in scores
 # key points:
+
 # 17 possessions in a typical IRB 7s match
 # the winner averages 9 possessions while the loser averages 8
 # each team gets approximately 4 scoring opportunities per half in
 # a team with a 1 possession advantage wins 80% of the time
 # a team with a 2 possession advantage wins 90% of the time
 # a team with a 3 possession advantage wins 98% of the time
+
 # in x$ of matches the teams had equal possessions
 # sources of additional possessions include reclaims and holding ball
 # when time expires -- either to score or run out the clock
@@ -160,3 +233,6 @@ wpm <- all_scores %>%
 # and taking from opponents, or holding at the end of half
 # did not discuss possession efficiency, which is ratio of scoring
 # to non-scoring possessions
+
+# efficiency
+
---
+++ 08_poss/wpm.R
@@ -0,0 +1,133 @@
+library("dplyr")
+library("ggplot2")
+library("grid")
+library("gridExtra")
+source('~/rugby7s/func/poss_func.R')
+
+# TODO
+# Add ties to the analysis
+# Add match count to winning possession margin
+
+if (!file.exists("ws72014_poss.csv")) {
+  download.file("https://raw.githubusercontent.com/jliberma/rugby7s/master/data/ws72014_possession_stats.csv", 
+                destfile="ws72014_poss.csv", method="curl")
+}
+
+ws7s_poss <- read.csv("ws72014_poss.csv", header=TRUE, stringsAsFactors=FALSE)
+
+poss.per.match <- sapply(split(ws7s_poss,ws7s_poss$SERIES.MATCH), 
+                         function(x) nrow(x))
+
+summary(poss.per.match)
+
+# ppm without finals
+ws7s_poss_nofinals <- ws7s_poss %>%
+  filter(!MATCH==45)
+poss.per.match.nofinals <- sapply(split(ws7s_poss_nofinals,ws7s_poss_nofinals$SERIES.MATCH), 
+                                  function(x) nrow(x))
+summary(poss.per.match.nofinals)
+# MAX 31 -> 28
+# Mean 17.02 -> 16.89
+# Median stays same
+
+# ppm finals only
+ws7s_poss_finals <- ws7s_poss %>%
+  filter(MATCH==45)
+poss.per.match.finals <- sapply(split(ws7s_poss_finals,ws7s_poss_finals$SERIES.MATCH), 
+                                function(x) nrow(x))
+summary(poss.per.match.finals)
+# finals range: 15-31
+# Median 15/17/19 -> 18/22/27
+# Mean: ~17 -> ~23
+
+# histogram of possessions per match
+ppm <- qplot(poss.per.match, binwidth=1) + 
+  geom_vline(xintercept=median(poss.per.match)+.5, colour="red") +
+  theme(text = element_text(family = "Trebuchet MS", color="#666666")) +
+  theme(plot.title = element_text(face="bold", size=24, hjust=0)) +
+  xlab("Possessions") + ylab("Count")
+
+# cumulative distribution function of possessions per match
+cdf <- data.frame(x=c(poss.per.match,poss.per.match.nofinals),
+                  ggg=factor(rep(1:2, c(length(poss.per.match),
+                                        length(poss.per.match.nofinals)))))
+cdf_plot <- ggplot(cdf, aes(x, colour=ggg)) +
+  theme(legend.position = c(0.8, 0.3)) +
+  xlab("Possessions") + ylab("Probability") + 
+  stat_ecdf(geom="step") +
+  theme(text = element_text(family = "Trebuchet MS", color="#666666")) +
+  theme(plot.title = element_text(face="bold", size=24, hjust=0)) +
+  scale_y_continuous(position = "right") +
+  scale_colour_hue(name="Legend", labels=c('FINALS','NOFINALS'))
+
+# plot histogram and cdf side by side
+title1=textGrob("Possessions per match", gp=gpar(fontface="bold", 
+                                                 fontfamily="Trebuchet MS",
+                                                 fontsize=24, col="#666666"))
+caption1=textGrob("Source: http://starting7s.com", gp=gpar(fontsize=10,
+                                                           fontfamily="Trebuchet MS",
+                                                           col="#666666"))
+
+grid.arrange(ppm, cdf_plot, ncol=2, 
+             top = title1,
+             bottom = caption1
+             )
+
+all_scores <- match_score_poss(ws7s_poss)
+
+tie_scores <- all_scores %>% 
+  filter(points.1==points.2) %>%
+  transmute(abs(count.1-count.2))
+
+wpm <- all_scores %>% 
+  filter(! points.1 == points.2) %>%
+  mutate(wpm = ifelse(points.1 > points.2, 
+                      count.1-count.2, 
+                      count.2-count.1), 
+         pos = wpm >=0) %>%
+  select(match, wpm, pos) %>%
+  arrange(wpm)
+
+summary(wpm[,2])
+
+# plot winning possession margin
+wpm1 <- ggplot(wpm, aes(x=match, y=wpm, fill=pos)) +
+  geom_bar(stat="identity", position="identity") +
+  geom_hline(yintercept=0, colour="grey") +
+  labs(title="Winning Possession Margin", subtitle="World Rugby Sevens Series 2014-2015", 
+       y="Possession Margin", x="", caption="Source: http://starting7s.com") +
+  theme(text = element_text(family = "Trebuchet MS", color="#666666")) +
+  theme(plot.title = element_text(face="bold", size=24, hjust=0)) +
+  guides(fill=F) + 
+  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
+print(wpm1)
+
+# calculate win probability by possession margin
+output <- matrix(ncol=4, nrow=7)
+for (i in 1:7)
+{
+  j <- round(sum(wpm$wpm==i)/(sum(abs(wpm$wpm)==i) +
+                                length(tie_scores[tie_scores==i])),2) 
+  output[i,1] <- i
+  output[i,2] <- j
+  output[i,3] <- round(sum(wpm$wpm==i),2)
+  output[i,4] <- round(sum(abs(wpm$wpm)==i) + length(tie_scores[tie_scores==i]),2)
+}
+output <- data.frame(output)
+colnames(output)<-c("margin","prob","win","total")
+
+# create a bar plot of win probability by possession margin
+# TODO: change to a stacked bar plot with won/(total-won) & labeled with probability
+nnp<-ggplot(data=output, aes(x=margin, y=prob)) +
+  geom_bar(stat="identity", width=0.75, fill="steelblue") +
+  geom_text(aes(label=total), position=position_dodge(width=0.8), hjust=1.5) +
+  scale_x_continuous(breaks=seq(1:7)) +
+  annotate("rect", xmin = 3.5, xmax = 7.5, ymin = -.009, ymax = 1.01,
+             alpha = .3) +
+  annotate("text", x = 5.5, y = .5, label = "Possession advantage >= 4 won 29/29") +
+  coord_flip() +
+  labs(title="Win Probability by Possession Advantage", subtitle="2014-2015 World Rugby Sevens Series", 
+       y="Win Probability", x="Possession Advantage", caption="Source: http://starting7s.com") +
+  theme(text = element_text(family = "Trebuchet MS", color="#666666")) +
+  theme(plot.title = element_text(face="bold", size=24, hjust=0))
+print(nnp)
